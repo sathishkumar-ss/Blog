46651fe349264f50c04e86b147174923edaa2446
sathishkumar-ss <sathishkumar97902@gmail.com>
2025-04-15 13:04:07 +0530

    Initial Commit

--- src/main/java/com/blogapp/blog/model/Post.java
+++ src/main/java/com/blogapp/blog/model/Post.java
@@ -1,5 +1,22 @@
 package com.blogapp.blog.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class Post {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User createdBy;
 }
--- src/main/java/com/blogapp/blog/model/User.java
+++ src/main/java/com/blogapp/blog/model/User.java
@@ -1,5 +1,17 @@
 package com.blogapp.blog.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
 }
