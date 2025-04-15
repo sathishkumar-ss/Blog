96eaf3a0971fb25e941d564a5abaaf9b3e03a7de
sathishkumar-ss <sathishkumar97902@gmail.com>
2025-04-15 12:30:03 +0530

    Added props

--- src/main/java/com/blogapp/blog/demo.java
+++ src/main/java/com/blogapp/blog/demo.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class demo {
-    @GetMapping("/welcome")
-    public String welcome(){
-        return "Starting";
+    @GetMapping("/blog")
+    public String blog(){
+        return "Changed application properties for postgres";
     }
     
 }
