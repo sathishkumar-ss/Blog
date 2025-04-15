deff5fcd5da4fec47215a08cadd8864ca22943ad
sathishkumar-ss <sathishkumar97902@gmail.com>
2025-04-15 13:05:54 +0530

    Code commit

--- src/main/java/com/blogapp/blog/repository/PostRepository.java
+++ src/main/java/com/blogapp/blog/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.blogapp.blog.repository;
 
-public class PostRepository {
-    
-}
+import com.blogapp.blog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {}
