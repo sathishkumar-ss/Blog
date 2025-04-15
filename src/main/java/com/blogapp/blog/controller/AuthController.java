6373684ef50db51da10fbc9c9ebd1a82380708e4
sathishkumar-ss <sathishkumar97902@gmail.com>
2025-04-15 13:15:21 +0530

    Code commit

--- src/main/java/com/blogapp/blog/controller/AuthController.java
+++ src/main/java/com/blogapp/blog/controller/AuthController.java
@@ -1,5 +1,25 @@
 package com.blogapp.blog.controller;
 
+import com.blogapp.blog.model.User;
+import com.blogapp.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
-    
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        boolean valid = userService.validateUser(user.getUsername(), user.getPassword());
+        return valid ? "Login success" : "Invalid credentials";
+    }
 }
--- src/main/java/com/blogapp/blog/controller/PostController.java
+++ src/main/java/com/blogapp/blog/controller/PostController.java
@@ -1,5 +1,35 @@
 package com.blogapp.blog.controller;
 
+import com.blogapp.blog.model.Post;
+import com.blogapp.blog.model.User;
+import com.blogapp.blog.service.PostService;
+import com.blogapp.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-    
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public Object createPost(@RequestBody Post post, @RequestParam String username, @RequestParam String password) {
+        if (!userService.validateUser(username, password)) {
+            return "Unauthorized";
+        }
+        User user = userService.getByUsername(username).get();
+        return postService.create(post, user);
+    }
+
+    @GetMapping
+    public List<Post> getAll() {
+        return postService.getAll();
+    }
 }
