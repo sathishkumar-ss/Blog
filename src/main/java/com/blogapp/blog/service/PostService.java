c9e61307c08554b110837c81f643c06dd4b5b37d
sathishkumar-ss <sathishkumar97902@gmail.com>
2025-04-15 13:13:52 +0530

    Code commit

--- src/main/java/com/blogapp/blog/service/PostService.java
+++ src/main/java/com/blogapp/blog/service/PostService.java
@@ -1,5 +1,27 @@
 package com.blogapp.blog.service;
 
+import com.blogapp.blog.model.Post;
+import com.blogapp.blog.model.User;
+import com.blogapp.blog.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class PostService {
-    
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post create(Post post, User user) {
+        post.setCreatedBy(user);
+        post.setCreatedAt(LocalDateTime.now());
+        return postRepository.save(post);
+    }
+
+    public List<Post> getAll() {
+        return postRepository.findAll();
+    }
 }
--- src/main/java/com/blogapp/blog/service/UserService.java
+++ src/main/java/com/blogapp/blog/service/UserService.java
@@ -1,5 +1,29 @@
 package com.blogapp.blog.service;
 
+import com.blogapp.blog.model.User;
+import com.blogapp.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User register(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean validateUser(String username, String password) {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        return optionalUser.isPresent() &&
+                optionalUser.get().getPassword().equals(password);
+    }
+
+    public Optional<User> getByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
