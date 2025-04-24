5df6708014a40b3b1176ed8acb21a9f821c4edbd
sathishkumar-ss <sathishkumar97902@gmail.com>
2025-04-24 18:47:18 +0530

    import commanded

--- src/main/java/com/blogapp/blog/security/JwtTokenUtil.java
+++ src/main/java/com/blogapp/blog/security/JwtTokenUtil.java
@@ -2,7 +2,7 @@ package com.blogapp.blog.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
