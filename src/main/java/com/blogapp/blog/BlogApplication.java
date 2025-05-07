2374c09050411844f6d7484c305f5e35808fcd72
sathishkumar-ss <sathishkumar97902@gmail.com>
2025-05-07 17:36:52 +0530

    code commit

--- src/main/java/com/blogapp/blog/BlogApplication.java
+++ src/main/java/com/blogapp/blog/BlogApplication.java
@@ -2,7 +2,7 @@ package com.blogapp.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//without react
 @SpringBootApplication
 public class BlogApplication {
 
