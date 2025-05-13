6f6a797cc640c35d2e538288ab41ed56f93a627b
sathishkumar-ss <sathishkumar97902@gmail.com>
2025-05-13 18:01:14 +0530

    Creating a utility class for Markdown processing for markdown text in the folder util.

---
+++ src/main/java/com/blogapp/blog/util/MarkdownProcessor.java
@@ -0,0 +1,48 @@
+package com.blogapp.blog.util;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.ext.heading.anchor.HeadingAnchorExtension;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class MarkdownProcessor {
+
+    private final Parser parser;
+    private final HtmlRenderer renderer;
+
+    public MarkdownProcessor() {
+        List<org.commonmark.Extension> extensions = Arrays.asList(
+            TablesExtension.create(),
+            HeadingAnchorExtension.create()
+        );
+        
+        parser = Parser.builder()
+                .extensions(extensions)
+                .build();
+                
+        renderer = HtmlRenderer.builder()
+                .extensions(extensions)
+                .build();
+    }
+
+    /**
+     * Converts Markdown text to HTML
+     * 
+     * @param markdown The Markdown text to convert
+     * @return The HTML representation of the Markdown text
+     */
+    public String convertMarkdownToHtml(String markdown) {
+        if (markdown == null) {
+            return "";
+        }
+        
+        Node document = parser.parse(markdown);
+        return renderer.render(document);
+    }
+} 
